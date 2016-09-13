e2b4c25dedd7988a1641f630ebdf2f8fbc1b2ebf
Alfredo Hinojosa <alfhh4@gmail.com>
2016-09-13 10:30:20 -0500

    Reset of Angular libraries

---
+++ Angular/app/app.component.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by ahinojosa on 13/09/16.
+ */
+
+import { Component } from '@angular/core';
+@Component({
+    selector: 'my-app',
+    template: '<h1>My First Angular 2 App</h1>'
+})
+export class AppComponent { }
---
+++ Angular/app/main.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by ahinojosa on 13/09/16.
+ */
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+const platform = platformBrowserDynamic();
+
+platform.bootstrapModule(AppModule);
