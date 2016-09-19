430228c65768aa3ee84071aa5a2a66890344b7cd
David-Delgadillo <daviddelgadillo.95@gmail.com>
2016-09-19 01:31:53 -0500

    Adding the correct style for the homepage

--- Angular/app/principal-components/login.component.ts
+++ Angular/app/principal-components/login.component.ts
@@ -8,33 +8,52 @@ import {AuthenticationService, User} from '../services/authentication.service'
     selector: 'login-form',
     providers: [AuthenticationService],
     template: `
-        <div class="container" >
-            <div class="title">
-                Welcome
-            </div>
-            <div class="panel-body">
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input [(ngModel)]="user.email" id="email"
-                            type="email" class="validate">
-                        <label for="email">Email</label>
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="input-field col s12">
-                        <input [(ngModel)]="user.password" id="password"
-                            type="password" class="validate">
-                        <label for="password">Password</label>
-                    </div>
-                </div>
-
-                <span>{{errorMsg}}</span>
-                <button (click)="login()"
-                    class="btn waves-effect waves-light"
-                    type="submit" name="action">Login</button>
-            </div>
+        <link type="text/css" rel="stylesheet" href="styles/estilosHomePage.css"/>
+        <nav style="height:80px">
+           <div class="nav-wrapper #4a148c purple darken-4">
+               <a href="" class="brand-logo" id='logo'>Logo</a>
+           </div>
+        </nav>
+        <div class="container" id="main">
+              <div class="row">
+               <div class="col s6">
+                   <img src="styles/mientras.jpg" style="width:324px;height:258px; padding-top: 0px;">
+               </div>
+               <div class="col s6">
+                   <h2>Welcome</h2>
+                   <br><br>
+                   <form>
+                       <div class="row">
+                            <div class="input-field col s12">
+                                <input [(ngModel)]="user.email" id="email"
+                                    type="email" class="validate">
+                                <label for="email">Email</label>
+                            </div>
+                       </div>
+                       <div class="row">
+                           <div class="input-field col s12">
+                                <input [(ngModel)]="user.password" id="password"
+                                    type="password" class="validate">
+                                <label for="password">Password</label>
+                           </div>
+                       </div>
+                       <a href="http://tutoring247.mty.itesm.mx/"><h6>Forgot your password?</h6></a>
+                       <br><br>
+                       <span>{{errorMsg}}</span>
+                       <button (click)="login()" 
+                            class="btn waves-effect waves-light btn #4a148c purple darken-4 right"
+                            type="submit" name="action">Login</button>
+                   </form>
+               </div>
+              </div>
         </div>
+        <footer class="page-footer N/A transparent">
+          <div class="footer-copyright N/A transparent">
+            <div class="container" id="footer">
+                <img src="styles/tec.png" class="right">
+            </div>
+          </div>
+        </footer>
     	`
 })
 
