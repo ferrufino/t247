193d521548fedd3feb5c55556352731a4045b2dc
Iker Arbulu Lozano <ikerarbulu@gmail.com>
2016-09-23 08:05:18 -0500

    Primera prueba con angular jalando falta hacerla funcionar con flask

--- Angular/app/services/authentication.service.js
+++ Angular/app/services/authentication.service.js
@@ -13,11 +13,36 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var router_1 = require('@angular/router');
+require('rxjs/add/operator/toPromise');
+require('rxjs/add/operator/map');
+var http_1 = require('@angular/http');
+var InMemoryDataService = (function () {
+    function InMemoryDataService() {
+    }
+    InMemoryDataService.prototype.createDb = function () {
+        var heroes = [
+            { id: 11, name: 'Mr. Nice' },
+            { id: 12, name: 'Narco' },
+            { id: 13, name: 'Bombasto' },
+            { id: 14, name: 'Celeritas' },
+            { id: 15, name: 'Magneta' },
+            { id: 16, name: 'RubberMan' },
+            { id: 17, name: 'Dynama' },
+            { id: 18, name: 'Dr IQ' },
+            { id: 19, name: 'Magma' },
+            { id: 20, name: 'Tornado' }
+        ];
+        return { heroes: heroes };
+    };
+    return InMemoryDataService;
+}());
+exports.InMemoryDataService = InMemoryDataService;
 var User = (function () {
-    function User(email, password, roles) {
+    function User(email, password, roles, token) {
         this.email = email;
         this.password = password;
         this.roles = roles;
+        this.token = token;
     }
     return User;
 }());
@@ -27,31 +52,41 @@ var users = [
     new User('root@gmail.com', 'root', ['user'])
 ];
 var AuthenticationService = (function () {
-    function AuthenticationService(_router) {
+    function AuthenticationService(_router, http) {
         this._router = _router;
+        this.http = http;
+        this.loggedIn = false;
+        this.loginUrl = 'http://localhost:5000/api/login';
+        this.headers = new http_1.Headers({ 'Content-Type': 'application/json' });
+        this.loggedIn = !!sessionStorage.getItem('auth_token');
     }
     AuthenticationService.prototype.logout = function () {
-        localStorage.removeItem("user");
+        sessionStorage.removeItem("email_user");
+        this.loggedIn = false;
         this._router.navigate(['login']);
     };
     AuthenticationService.prototype.login = function (user) {
-        var authenticatedUser = users.find(function (u) { return u.email === user.email; });
-        if (authenticatedUser && authenticatedUser.password === user.password) {
-            //localStorage.setItem("user", authenticatedUser);
-            localStorage.setItem("user", JSON.stringify(authenticatedUser));
-            this._router.navigate(['']);
-            return true;
-        }
-        return false;
+        var _this = this;
+        return this.http
+            .post(this.loginUrl, JSON.stringify({ email: user.email, password: user.password }), this.headers)
+            .map(function (res) { return res.json(); })
+            .map(function (res) {
+            if (res.success) {
+                sessionStorage.setItem('auth_token', res.auth_token);
+                sessionStorage.setItem('email_user', user.email);
+                _this.loggedIn = true;
+            }
+            return res.success;
+        });
     };
     AuthenticationService.prototype.checkCredentials = function () {
-        if (localStorage.getItem("user") === null) {
+        if (!this.loggedIn) {
             this._router.navigate(['login']);
         }
     };
     AuthenticationService = __decorate([
         core_1.Injectable(), 
-        __metadata('design:paramtypes', [router_1.Router])
+        __metadata('design:paramtypes', [router_1.Router, http_1.Http])
     ], AuthenticationService);
     return AuthenticationService;
 }());
--- Angular/app/services/authentication.service.ts
+++ Angular/app/services/authentication.service.ts
@@ -4,12 +4,37 @@
 
 import {Injectable} from '@angular/core';
 import {Router} from '@angular/router';
+import 'rxjs/add/operator/toPromise';
+import 'rxjs/add/operator/map';
+
+import { Headers, Http } from '@angular/http';
+
+
+import { InMemoryDbService } from 'angular2-in-memory-web-api';
+export class InMemoryDataService implements InMemoryDbService {
+  createDb() {
+    let heroes = [
+      {id: 11, name: 'Mr. Nice'},
+      {id: 12, name: 'Narco'},
+      {id: 13, name: 'Bombasto'},
+      {id: 14, name: 'Celeritas'},
+      {id: 15, name: 'Magneta'},
+      {id: 16, name: 'RubberMan'},
+      {id: 17, name: 'Dynama'},
+      {id: 18, name: 'Dr IQ'},
+      {id: 19, name: 'Magma'},
+      {id: 20, name: 'Tornado'}
+    ];
+    return {heroes};
+  }
+}
 
 export class User {
     constructor(
-        public email: string,
-        public password: string,
-        public roles : [string]) { }
+        public email?: string,
+        public password?: string,
+        public roles?: [string],
+        public token?: string) { }
 }
 
 var users = [
@@ -20,29 +45,45 @@ var users = [
 @Injectable()
 export class AuthenticationService {
 
+  private loggedIn = false;
+
+  private loginUrl = 'http://localhost:5000/api/login';
+
+  private headers = new Headers({'Content-Type': 'application/json'});
+
     constructor(
-        private _router: Router){}
+        private _router: Router, private http: Http){
+          this.loggedIn = !!sessionStorage.getItem('auth_token');
+        }
 
     logout() {
-        localStorage.removeItem("user");
+        sessionStorage.removeItem("email_user");
+        this.loggedIn = false;
         this._router.navigate(['login']);
     }
 
     login(user){
-        var authenticatedUser = users.find(u => u.email === user.email);
-        if (authenticatedUser && authenticatedUser.password === user.password){
-            //localStorage.setItem("user", authenticatedUser);
-            localStorage.setItem("user",JSON.stringify(authenticatedUser));
-            this._router.navigate(['']);
-            return true;
+      return this.http
+      .post(
+        this.loginUrl,
+        JSON.stringify({ email:user.email, password:user.password }),
+        this.headers
+      )
+      .map(res => res.json())
+      .map((res) => {
+        if (res.success) {
+          sessionStorage.setItem('auth_token', res.auth_token);
+          sessionStorage.setItem('email_user',user.email);
+          this.loggedIn = true;
         }
-        return false;
 
+        return res.success;
+      });
     }
 
     checkCredentials(){
-        if (localStorage.getItem("user") === null){
-            this._router.navigate(['login']);
-        }
+      if (!this.loggedIn){
+        this._router.navigate(['login']);
+      }
     }
 }
