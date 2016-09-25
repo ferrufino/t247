375e8c4811c3555fd965c11c6fd85c89d7df5375
Alfredo Hinojosa <alfhh4@gmail.com>
2016-09-25 10:52:43 -0500

    Reformat of Angular App with CLI

---
+++ Angular/t247-app/e2e/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, element, by } from 'protractor/globals';
+
+export class T247AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('app-root h1')).getText();
+  }
+}
