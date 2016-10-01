0507f6801140aea1b2d41b49862cb17526cc6cbe
Iker Arbulu Lozano <ikerarbulu@gmail.com>
2016-10-01 12:53:30 -0500

    Deleted map and js files from repo, these are generated with typescript

--- Angular/app/main.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ahinojosa on 13/09/16.
- */
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var app_module_1 = require('./app.module');
-var platform = platform_browser_dynamic_1.platformBrowserDynamic();
-platform.bootstrapModule(app_module_1.AppModule);
-//# sourceMappingURL=main.js.map
