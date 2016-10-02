7c7f61b2ca079391677333afbef7d5828d8c0247
ferrufino <gustavo.fdlf@gmail.com>
2016-10-02 01:41:10 -0500

    adds pipe filter and example

--- Angular/t247-app/src/app/app.module.ts
+++ Angular/t247-app/src/app/app.module.ts
@@ -2,17 +2,15 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import {routing} from "./app.routing";
-
 import { AppComponent }   from './app.component';
 import {LoginComponent} from "./principal-components/login-component/login.component";
 import {HomeComponent} from "./principal-components/home-component/home.component";
-import {MyAssignments} from "./user-components/my-assignments.component";
 import {MyCourses} from "./professor-components/my-courses.component";
 import {ListOfProblems} from "./admin-components/list-of-problems.component";
-import {TopicsDashboard} from "./principal-components/topics-dashboard.component";
 import {CreateProblem} from "./general-components/create-problem.component";
 import { StudentComponentsComponent } from './student-components/student-components.component';
-import { TopicsDashboardComponent } from './student-components/topics-dashboard/topics-dashboard.component';
+import { GenericTableComponent } from './general-components/generic-table/generic-table.component';
+import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
     imports:      [
@@ -24,13 +22,12 @@ import { TopicsDashboardComponent } from './student-components/topics-dashboard/
         AppComponent,
         LoginComponent,
         HomeComponent,
-        MyAssignments,
         MyCourses,
         ListOfProblems,
-        TopicsDashboard,
         CreateProblem,
         StudentComponentsComponent,
-        TopicsDashboardComponent
+        GenericTableComponent,
+        FilterPipe
     ],
     bootstrap:    [ AppComponent ]
 })
---
+++ Angular/t247-app/src/app/general-components/generic-table/generic-table.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'generic-table',
+  templateUrl: './generic-table.component.html',
+  styleUrls: ['./generic-table.component.css']
+})
+export class GenericTableComponent {
+
+    myvalues = ['Milk', 'Bread', 'Gustavo', 'Ferrufino', 'Mom', 'ea', 'earing'];
+
+}
