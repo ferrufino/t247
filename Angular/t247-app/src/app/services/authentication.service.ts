802bc442e3f861d48c1149379c60406bc37f94bf
Eduardo Zardain <ejzardainc@gmail.com>
2016-10-05 20:18:11 -0500

    Fixes indentation in authentication_service

--- Angular/t247-app/src/app/services/authentication.service.ts
+++ Angular/t247-app/src/app/services/authentication.service.ts
@@ -11,7 +11,7 @@ export class AuthenticationService {
   private loggedIn = false;
 
   private loginUrl = 'http://localhost:5000/api/users/login';
-    private logoutUrl = 'http://localhost:5000/api/users/logout';
+  private logoutUrl = 'http://localhost:5000/api/users/logout';
 
   private rolesUrl = 'http://localhost:5000/api/users/role';
 
@@ -25,9 +25,11 @@ export class AuthenticationService {
     logout() {
         let body = JSON.stringify({"token":sessionStorage.getItem("auth_token")});
         let options = new RequestOptions({ headers: this.headers });
-        this.http.post(this.logoutUrl, {"token":sessionStorage.getItem("auth_token")}, this.headers)
+        this.http.post(this.logoutUrl, body, options)
             .map(res => res.json())
-            .map((res) => { console.log("response from logout:"+res);});
+            .map((res) => {
+              console.log("response from logout:"+res);
+            });
 
         sessionStorage.removeItem("email_user");
         sessionStorage.removeItem("auth_token");
@@ -41,33 +43,32 @@ export class AuthenticationService {
     }
 
     login(user){
-      return this.http
-      .post(
+        return this.http
+        .post(
         this.loginUrl,
         { "email":user.email, "password":user.password },
         this.headers
-      )
-      .map(res => res.json())
-      .map((res) => {
+        )
+        .map(res => res.json())
+        .map((res) => {
         if (res.token) {
-          sessionStorage.setItem('userJson',JSON.stringify(new User(user.email,res.id,res.name,res.lastName,res.enrollment)));
-          sessionStorage.setItem('auth_token', res.token);
-          sessionStorage.setItem('email_user',user.email);
-          res.role = 'admin';
-          let roles = ['student'];
-          if(res.role == 'admin'){
+            sessionStorage.setItem('userJson',JSON.stringify(new User(user.email,res.id,res.name,res.lastName,res.enrollment)));
+            sessionStorage.setItem('auth_token', res.token);
+            sessionStorage.setItem('email_user',user.email);
+            res.role = 'admin';
+            let roles = ['student'];
+            if(res.role == 'admin'){
             roles.push('admin','professor');
-          }
-          else if(res.role == 'professor'){
+            }
+            else if(res.role == 'professor'){
             roles.push('professor');
-          }
-          sessionStorage.setItem('roles',JSON.stringify(roles));
-          this.loggedIn = true;
-          this._router.navigate(['']);
+            }
+            sessionStorage.setItem('roles',JSON.stringify(roles));
+            this.loggedIn = true;
+            this._router.navigate(['']);
         }
-
         return res.token;
-      });
+        });
     }
 
     checkCredentials(){
