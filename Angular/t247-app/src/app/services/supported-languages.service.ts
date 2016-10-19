196dff33a17db9d7260c101d81be4eb0e6576e52
Alfredo Hinojosa <alfhh4@gmail.com>
2016-10-18 22:06:53 -0500

    Added extension property for the ProgLanguage class

--- Angular/t247-app/src/app/services/supported-languages.service.ts
+++ Angular/t247-app/src/app/services/supported-languages.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from '@angular/core';
 export class ProgLanguage {
     constructor(
         public name: string,
-        public value: string) { }
+        public value: string,
+        public extension: string) { }
 }
 
 var languges: ProgLanguage[] = [
-    new ProgLanguage('C++','cpp'),
-    new ProgLanguage('Java','java')
+    new ProgLanguage('C++','cpp', '.cpp'),
+    new ProgLanguage('Java','java', '.java')
 ];
 
 @Injectable()
