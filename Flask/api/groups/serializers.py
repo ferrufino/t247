f83243882be140400ad45b84e47e7962bf5dc8f6
Eduardo Zardain <ejzardainc@gmail.com>
2016-10-20 13:01:29 -0500

    Adds nested group model in group serialization

--- Flask/api/groups/serializers.py
+++ Flask/api/groups/serializers.py
@@ -1,11 +1,12 @@
 from flask_restplus import fields
 from api.restplus import api
+from api.courses.serializers import course
 
 group = api.model('Group', {
     'id': fields.Integer(required=True, description='Group id'),
     'period': fields.String(required=True, description='Group period'),
     'professor_id': fields.Integer(required=True, description='Group professor id'),
-    'course_id': fields.Integer(required=True, description='Group course id')
+    'course': fields.Nested(course)
 })
 
 group_creation = api.model('Group', {
