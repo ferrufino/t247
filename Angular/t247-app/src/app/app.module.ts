f516c58dceabba185d79279f7a84f2b2e4933a40
ferrufino <gustavo.fdlf@gmail.com>
2016-10-22 13:57:38 -0500

    Adds submission of a problem component

--- Angular/t247-app/src/app/app.module.ts
+++ Angular/t247-app/src/app/app.module.ts
@@ -13,23 +13,23 @@ import {SubmissionsComponent} from "./submissions/submissions.component";
 import {ProfileComponent} from "./general-components/profile/profile.component";
 import {ViewUsersComponent} from "./admin-components/view-users/view-users.component";
 import {CreateProblem} from "./general-components/create-problem/create-problem.component";
+import {ProblemComponent} from "./general-components/problem/problem.component";
+import {StudentComponentsComponent} from './student-components/student-components.component';
+import {GenericTableComponent} from './general-components/generic-table/generic-table.component';
+import {Tab} from './general-components/tab/tab.component';
+import {Tabs} from './general-components/tabs/tabs.component';
+import {TestCasesCreatorComponent} from './general-components/create-problem/test-cases-creator.component';
 
-import { StudentComponentsComponent } from './student-components/student-components.component';
-import { GenericTableComponent } from './general-components/generic-table/generic-table.component';
-import { Tab } from './general-components/tab/tab.component';
-import { Tabs } from './general-components/tabs/tabs.component';
-import { TestCasesCreatorComponent } from './general-components/create-problem/test-cases-creator.component';
 
-
-import { KeysPipe } from './pipes/keys.pipe';
-import { FilterPipe } from './pipes/filter.pipe';
-import { FilterUsersPipe } from './pipes/filter-users.pipe';
-import { FilterProblemsPipe } from './pipes/filter-problems.pipe';
-import { FilterAssignmentsPipe } from './pipes/filter-assignments.pipe';
-import { FilterSubmissionsPipe } from './pipes/filter-submissions.pipe';
-import { FilterGroupsPipe } from './pipes/filter-groups.pipe';
-import { FilterCoursesPipe } from './pipes/filter-courses.pipe';
-import { FilterTopicsPipe } from './pipes/filter-topics.pipe';
+import {KeysPipe} from './pipes/keys.pipe';
+import {FilterPipe} from './pipes/filter.pipe';
+import {FilterUsersPipe} from './pipes/filter-users.pipe';
+import {FilterProblemsPipe} from './pipes/filter-problems.pipe';
+import {FilterAssignmentsPipe} from './pipes/filter-assignments.pipe';
+import {FilterSubmissionsPipe} from './pipes/filter-submissions.pipe';
+import {FilterGroupsPipe} from './pipes/filter-groups.pipe';
+import {FilterCoursesPipe} from './pipes/filter-courses.pipe';
+import {FilterTopicsPipe} from './pipes/filter-topics.pipe';
 
 import "materialize-css";
 import {MaterializeModule} from "angular2-materialize";
@@ -38,42 +38,43 @@ import {GroupsService} from "./services/groups.service";
 import {TopicsService} from "./services/topics.service";
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    ReactiveFormsModule,
-    routing,
-    MaterializeModule
-  ],
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    HomeComponent,
-    MyCourses,
-    ListOfProblems,
-    TopicsDashboardComponent,
-    CreateProblem,
-    SubmissionsComponent,
-    ProfileComponent,
-    ViewUsersComponent,
-    StudentComponentsComponent,
-    GenericTableComponent,
-    Tab,
-    Tabs,
-    FilterPipe,
-    TestCasesCreatorComponent,
-    KeysPipe,
-    FilterUsersPipe,
-    FilterProblemsPipe,
-    FilterAssignmentsPipe,
-    FilterSubmissionsPipe,
-    FilterGroupsPipe,
-    FilterCoursesPipe,
-    FilterTopicsPipe
-  ],
-  bootstrap: [AppComponent],
-  providers: [CoursesService, GroupsService, TopicsService]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpModule,
+        ReactiveFormsModule,
+        routing,
+        MaterializeModule
+    ],
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        HomeComponent,
+        MyCourses,
+        ListOfProblems,
+        TopicsDashboardComponent,
+        CreateProblem,
+        ProblemComponent,
+        SubmissionsComponent,
+        ProfileComponent,
+        ViewUsersComponent,
+        StudentComponentsComponent,
+        GenericTableComponent,
+        Tab,
+        Tabs,
+        FilterPipe,
+        TestCasesCreatorComponent,
+        KeysPipe,
+        FilterUsersPipe,
+        FilterProblemsPipe,
+        FilterAssignmentsPipe,
+        FilterSubmissionsPipe,
+        FilterGroupsPipe,
+        FilterCoursesPipe,
+        FilterTopicsPipe
+    ],
+    bootstrap: [AppComponent],
+    providers: [CoursesService, GroupsService, TopicsService]
 })
 export class AppModule {
 }
