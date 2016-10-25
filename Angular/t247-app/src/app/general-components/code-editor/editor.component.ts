20850cbb85a3cabe3ba7f60a3b28b26e1faf910a
ferrufino <gustavo.fdlf@gmail.com>
2016-10-25 17:56:32 -0500

    Adds Output property from code editor to problem submission

--- Angular/t247-app/src/app/general-components/code-editor/editor.component.ts
+++ Angular/t247-app/src/app/general-components/code-editor/editor.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Output, EventEmitter} from '@angular/core';
 import 'codemirror/mode/javascript/javascript';
 
 
@@ -12,20 +12,27 @@ if(youReallyThinkYouCanCode){
     selector: 'code-editor',
     template: `<div>
     <codemirror [(ngModel)]="code" [config]="config"></codemirror>
+    <button type="button" class="btn btn-success" (click)='onClick()'>Submit</button>
   </div>`,
 })
 export class EditorComponent {
 
-        // Configuration object
-        config = {
-            lineNumbers: true,
-            mode: {
-                name: 'javascript',
-                json: true
-            }
-        };
+    // Configuration object
+    config = {
+        lineNumbers: true,
+        mode: {
+            name: 'javascript',
+            json: true
+        }
+    };
 
-        // Sample code
-        code = sampleCode;
+    // Sample code
+    code = sampleCode;
+
+    @Output() codeToSubmit = new EventEmitter();
+
+    onClick() {
+        this.codeToSubmit.emit(this.code);
+    }
 
 }
