fa450038972c151697966eed8f424b5b8b4f0ddd
ferrufino <gustavo.fdlf@gmail.com>
2016-10-26 14:43:55 -0500

    Adds base for submit problem post

--- Angular/t247-app/src/app/general-components/submit-problem/submit-problem.component.ts
+++ Angular/t247-app/src/app/general-components/submit-problem/submit-problem.component.ts
@@ -1,54 +1,69 @@
-import { Component, OnInit } from '@angular/core';
-import {IMultiSelectOption,  IMultiSelectTexts, IMultiSelectSettings} from 'angular-2-dropdown-multiselect/src/multiselect-dropdown';
-//import {EvaluatorService} from "../../services/evaluator.service";
+import {Component, OnInit} from '@angular/core';
+import {
+    IMultiSelectOption,
+    IMultiSelectTexts,
+    IMultiSelectSettings
+} from 'angular-2-dropdown-multiselect/src/multiselect-dropdown';
+import {EvaluatorService} from "../../services/evaluator.service";
 
 @Component({
-  selector: 'submit-problem',
-  templateUrl: './submit-problem.component.html',
-  styleUrls: ['./submit-problem.component.css']
+    selector: 'submit-problem',
+    templateUrl: './submit-problem.component.html',
+    styleUrls: ['./submit-problem.component.css'],
+    providers: [EvaluatorService]
 })
 export class SubmitProblem {
-    /*constructor(private _httpProblemsService: EvaluatorService){
-        
-    }*/
-    progLangToSubmit: string;
-    private selectedOptions: number[];
-    private myOptions: IMultiSelectOption[] = [
-        { id: 1, name: 'C++' },
-        { id: 2, name: 'Java' },
+    constructor(private _httpProblemsService:EvaluatorService) {
+
+    }
+
+    progLangToSubmit;
+    private selectedOptions:number[];
+    private myOptions:IMultiSelectOption[] = [
+        {id: 1, name: 'C++'},
+        {id: 2, name: 'Java'},
     ];
 
-    private mySettings: IMultiSelectSettings = {
+    private mySettings:IMultiSelectSettings = {
 
         selectionLimit: 1,
         closeOnSelect: true,
         checkedStyle: 'checkboxes'
     }
-    private myTexts: IMultiSelectTexts = {
+    private myTexts:IMultiSelectTexts = {
         checked: 'checked',
         defaultTitle: 'Programming Languages'
     };
 
-    onChange($event){
-        if($event == 1){
+    onChange($event) {
+        if ($event == 1) {
             this.progLangToSubmit = "cpp";
-        }else{
+        } else {
             this.progLangToSubmit = "java";
         }
     }
 
     submitCode($event) {
         console.log($event);
-        /*string code = $event;
+
+        var codeFromEditor = $event;
         let codeObject = {
-            "code": code,
+            "code": $event,
             "language": this.progLangToSubmit,
             "problem_id": 5,
             "request_type": "submission",
-            "user_id": 0
-        }*/
+            "user_id": 2
+        }
+
+        this._httpProblemsService.submitProblem(codeFromEditor).subscribe(
+            data => {
+                console.log(data);
+            },
+            err => {
+                console.log(err);
+            }
+        );
     }
-    
-    
+
 
 }
