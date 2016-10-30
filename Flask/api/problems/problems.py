369df86db0d513c8a1b04450706b856d408cc4c6
msf1013 <msf1013@gmail.com>
2016-10-30 17:48:58 -0600

    Added API for listing problems by topic, indicating if user has solved, attempted or not a problem

--- Flask/api/problems/problems.py
+++ Flask/api/problems/problems.py
@@ -4,7 +4,7 @@ from flask import request, abort, jsonify, g
 from flask_restplus import Resource
 from api.problems.serializers import problem as api_problem
 from api.restplus import api
-from models import db, Problem
+from models import db, Problem, Topic, ProblemTopic
 
 log = logging.getLogger(__name__)
 
@@ -55,4 +55,32 @@ class ProblemDescription(Resource):
         print(descriptions)
         
         return descriptions
+
+@ns.route('/listbytopic/<int:user_id>/<int:topic_id>')
+@api.response(404, 'Problem not found.')
+class ProblemsByTopic(Resource):
+
+    def get(self, user_id, topic_id):
+        """
+        Returns list of problems by topic, indicating if the problem has been solved by user
+        """
+        
+        # Retrieve raw list of problems by topic
+        result = db.engine.execute("SELECT p.id, p.name, p.difficulty FROM Problem p, ProblemTopic pt WHERE p.id = pt.problem_id AND pt.topic_id = %d" % (topic_id))
+        
+        problems_list = []
+        
+        # Mark problem status (not attempted, attempted but wrong answer, or solved)
+        for problem in result:
+            # Problem not attempted
+            if (len(db.engine.execute("SELECT p.id FROM Problem p WHERE p.id = %d AND NOT EXISTS (SELECT s.id FROM Submission s WHERE p.id = s.problem_id)" % (problem[0])).fetchall()) > 0):
+                problems_list.append({'id' : problem[0], 'name' : problem[1], 'difficulty' : problem[2], 'status' : 'not_attempted'})
+            # Problem attempted but not solved
+            elif (len(db.engine.execute("SELECT p.id FROM Problem p WHERE p.id = %d AND NOT EXISTS (SELECT s.id FROM Submission s WHERE p.id = s.problem_id AND s.grade = 100)" % (problem[0])).fetchall()) > 0):
+                problems_list.append({'id' : problem[0], 'name' : problem[1], 'difficulty' : problem[2], 'status' : 'wrong_answer'})
+            # Problem solved
+            else:
+                problems_list.append({'id' : problem[0], 'name' : problem[1], 'difficulty' : problem[2], 'status' : 'accepted'})
+        
+        return problems_list
         
