523c004d8dd27c73d9d156490756f175bb45de18
Alfredo Hinojosa <alfhh4@gmail.com>
2016-11-02 20:46:49 -0600

    Problem creation fixes in Angular

--- Angular/t247-app/src/app/general-components/create-problem/TestCase.ts
+++ Angular/t247-app/src/app/general-components/create-problem/TestCase.ts
@@ -9,14 +9,14 @@
 
 export class TestCase {
 
-  public onDescription: boolean;
-  public content: string;
-  public output: string;
-  public feedback: string;
-  public status: string;
+  public is_sample: boolean; // This flag specifies if the test cases will be added to description or not
+  public input: string; // Input to test in the evaluator
+  public output: string; // The output generated for this problem
+  public feedback: string; // The feedback provided by the author of the problem
+  public status: string; // This field contains the status of the evaluation for this test case
 
   constructor(onDescription: boolean, status: string) {
-    this.onDescription = onDescription;
+    this.is_sample = onDescription;
     this.status = "successful run";
   }
 }
