7e7f7279c814cea17921ace702c2c2a4a113ee80
Eduardo Zardain <ejzardainc@gmail.com>
2016-11-02 18:56:32 -0600

    Fixes assignment queries to use user_id instead of student_id

--- Flask/api/assignments/assignments.py
+++ Flask/api/assignments/assignments.py
@@ -96,44 +96,14 @@ class AssignmentSubmissionSummary(Resource):
         group. Telling the number of attempts, as well as the best grade for
         the assignment so far
         """
-        # assignment = Assignment.query.filter(Assignment.id == id).one()
-        # problem = assignment.problem
-        # start_date = assignment.start_date
-        # due_date = assignment.due_date
-        # students = assignment.group.students
-
-        # submissions = []
-        # for student in students:
-        #     print(str(student.id))
-        #     submission = Submission.query.filter(
-        #         and_(Submission.problem_id == problem.id,
-        #              Submission.student_id == student.id,
-        #              Submission.created >= start_date,
-        #              Submission.created <= due_date)).order_by(
-        #         Submission.created.desc()).first()
-        #     submissions.append(submission)
-
-        # return submissions
-
         result = db.engine.execute("""
             SELECT u.id as student_id, (u.first_name || ' ' || u.last_name) as student_name, u.enrollment, COUNT(u.enrollment) as no_of_attempts, MAX(s.created) as date, MAX(s.grade) as grade
             FROM \"user\" u, submission s, enrollment e, assignment a
-            WHERE s.problem_id = a.problem_id AND a.id = %d AND u.id = e.student_id AND e.group_id = a.group_id AND s.student_id = u.id AND a.start_date <= s.created AND s.created <= a.due_date
+            WHERE s.problem_id = a.problem_id AND a.id = %d AND u.id = e.student_id AND e.group_id = a.group_id AND s.user_id = u.id AND a.start_date <= s.created AND s.created <= a.due_date
             GROUP BY u.id, u.enrollment;""" % (assignment_id)).fetchall()
 
         return result
 
-# @ns.route('/submissionslist/<int:assignment_id>/')
-# @api.response(404, 'Submission not found.')
-# class AssignmentSubmissionSummary(Resource):
-#     @api.marshal_list_with(assignment_submission_summary)
-#     def get(self, assignment_id):
-#         """
-#         Returns number of attempts and status of a submission
-#         """
-#         result = db.engine.execute("SELECT u.id as student_id, (u.first_name || ' ' || u.last_name) as student_name, u.enrollment, COUNT(u.enrollment) as no_of_attempts, MAX(s.created) as date, MAX(s.grade) as grade FROM \"user\" u, submission s, enrollment e, assignment a WHERE s.problem_id = a.problem_id AND a.id = %d AND u.id = e.student_id AND e.group_id = a.group_id AND s.student_id = u.id AND a.start_date <= s.created AND s.created <= a.due_date GROUP BY u.id, u.enrollment;" % (assignment_id)).fetchall()
-
-#         return result
 
 @ns.route('/studentsubmissionscode/<int:assignment_id>/<int:student_id>')
 @api.response(404, 'Submission not found.')
@@ -146,7 +116,7 @@ class AssignmentSubmissionCodeByStudent(Resource):
         result = db.engine.execute("""
             SELECT s.created as date, s.grade, s.code
             FROM submission s, enrollment e, assignment a
-            WHERE s.problem_id = a.problem_id AND a.id = %d AND e.student_id = s.student_id AND s.student_id = %d AND e.group_id = a.group_id AND a.start_date <= s.created AND s.created <= a.due_date
+            WHERE s.problem_id = a.problem_id AND a.id = %d AND e.student_id = s.user_id AND s.user_id = %d AND e.group_id = a.group_id AND a.start_date <= s.created AND s.created <= a.due_date
             ORDER BY s.created;""" % (assignment_id, student_id)).fetchall()
 
         return result
@@ -162,7 +132,7 @@ class AssignmentSubmissionCodeByStudent(Resource):
         result = db.engine.execute("""
             SELECT a.title, p.name as problem_name, p.difficulty, c.name as course_name, a.due_date, MAX(s.grade) as grade
             FROM problem p, course c, submission s, assignment a, \"group\" g, enrollment e
-            WHERE p.id = a.problem_id AND p.id = s.problem_id AND a.group_id = g.id AND g.course_id = c.id AND a.start_date <= NOW() AND NOW() <= a.due_date AND e.group_id = a.group_id AND e.student_id = s.student_id AND s.student_id = %d
+            WHERE p.id = a.problem_id AND p.id = s.problem_id AND a.group_id = g.id AND g.course_id = c.id AND a.start_date <= NOW() AND NOW() <= a.due_date AND e.group_id = a.group_id AND e.student_id = s.user_id AND s.user_id = %d
             GROUP BY a.title, p.name, p.difficulty, c.name, a.due_date
             ORDER BY a.due_date;""" % (student_id)).fetchall()
 
