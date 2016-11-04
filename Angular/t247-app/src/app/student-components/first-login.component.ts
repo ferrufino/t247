fb1a33f8a09ac6ecf7596bbfac71c8a5934dfef4
Omar Manjarrez <omjrrz@outlook.com>
2016-11-04 10:19:19 -0600

    added logic to first login component

--- Angular/t247-app/src/app/student-components/first-login.component.ts
+++ Angular/t247-app/src/app/student-components/first-login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from "@angular/forms"
 import {UsersService} from '../services/users.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-first-login',
@@ -9,11 +10,13 @@ import {UsersService} from '../services/users.service';
 })
 export class FirstLoginComponent implements OnInit {
 
-  constructor(private _authService: UsersService) { }
+  constructor(private _authService: UsersService,
+              private _router: Router) { }
 
   public userEdit;
 
   ngOnInit() {
+    this._authService.checkCredentials();
     // Getting data of the current user
     let userInfo = JSON.parse(sessionStorage.getItem("userJson"));
     this._authService.getUser(userInfo.id)
@@ -30,11 +33,13 @@ export class FirstLoginComponent implements OnInit {
     this.userEdit.first_name = form.value.first_name
     this.userEdit.last_name = form.value.last_name
     this.userEdit.email = form.value.email
+    this.userEdit.password = form.value.password
     this._authService.editUser(this.userEdit).subscribe((result) => {
       if (!result) {
         console.log("Fallo");
       }
       else{
+        this._router.navigate(['']);
         console.log(this.userEdit);
       }
     });
