bbfa0f1e2110cb3f7d33ab79d0a0972aa526fefb
ferrufino <gustavo.fdlf@gmail.com>
2016-11-19 15:19:45 -0600

    Fixes conditional in First time logged in

--- Flask/api/users/users.py
+++ Flask/api/users/users.py
@@ -73,7 +73,7 @@ class UserAuthentication(Resource):
         email = request.json.get('email')
         password = request.json.get('password')
         if verify_password(email, password):
-            if (g.user.first_name == None and g.user.last_name == None) or (g.user.first_name == "" and g.user.last_name == ""):
+            if ((g.user.first_name == None and g.user.last_name == None) or (g.user.first_name == "" and g.user.last_name == "")):
                 print("First time logged in")
                 return {'token': 'first_time'}, 104
             token = g.user.generate_auth_token()
