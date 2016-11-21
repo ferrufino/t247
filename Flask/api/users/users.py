ba6da7e91b1a674dd9234fd2149552ca41629dae
ferrufino <gustavo.fdlf@gmail.com>
2016-11-20 21:43:06 -0600

    Adds feedback on reponse code for first log in

--- Flask/api/users/users.py
+++ Flask/api/users/users.py
@@ -65,6 +65,7 @@ class UserCreation(Resource):
 @ns.route('/login')
 class UserAuthentication(Resource):
     @api.response(200, 'User succesfully logged')
+    @api.response(104, 'User first time logged')
     @api.expect(user_auth)
     def post(self):
         """
