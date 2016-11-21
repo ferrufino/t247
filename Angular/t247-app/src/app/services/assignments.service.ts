4e4198e69d7312d2e3a10cf402208566607fb3ec
ferrufino <gustavo.fdlf@gmail.com>
2016-11-21 12:31:39 -0600

    Formats assignment service

--- Angular/t247-app/src/app/services/assignments.service.ts
+++ Angular/t247-app/src/app/services/assignments.service.ts
@@ -9,7 +9,11 @@ export class AssignmentsService {
     private userURL:string = environment.apiURL + '/assignments/bystudent';
     private createURL:string = environment.apiURL + '/assignments/create';
 
-    private headers = new Headers({'Content-Type': 'application/json','Access-Control-Allow-Origin': 'localhost:4200', 'Authorization': localStorage.getItem('auth_token')});
+    private headers = new Headers({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'localhost:4200',
+        'Authorization': localStorage.getItem('auth_token')
+    });
 
     private options = new RequestOptions({headers: this.headers});
 
@@ -19,12 +23,13 @@ export class AssignmentsService {
     getAssignments() {
         return this.http.get(this.userURL, this.options).map((response:Response) => response.json());
     }
-  getAssignmentsByStudent(id) {
-    return this.http.get(
-        this.baseURL + 'bystudent/'+id,
-        this.options
-    ).map((response: Response) => response.json());
-  }
+
+    getAssignmentsByStudent(id) {
+        return this.http.get(
+            this.baseURL + 'bystudent/' + id,
+            this.options
+        ).map((response:Response) => response.json());
+    }
 
     getSubmissions(id) {
         return this.http
