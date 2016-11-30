5dd6a2a3c9adf2fd98e6203a70539d1e77869415
ferrufino <gustavo.fdlf@gmail.com>
2016-11-30 16:26:02 -0600

    Cleans stuff

--- Angular/t247-app/src/app/professor-components/group/group-form/group-form.component.ts
+++ Angular/t247-app/src/app/professor-components/group/group-form/group-form.component.ts
@@ -71,7 +71,6 @@ export class GroupFormComponent implements OnInit {
         }
         this.group.courseId = Number(this.group.courseId);
         this.group.enrollmentText = this.group.enrollmentText.replace(/\s/g, '');
-        alert(this.group.enrollmentText);
         this.group.enrollments = this.group.enrollmentText.split(",");
         this.group.professor = JSON.parse(localStorage.getItem('userJson')).id;
         if (llenado) {
