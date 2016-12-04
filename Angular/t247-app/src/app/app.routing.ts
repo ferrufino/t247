df292e06727176fe07b7e16ef0ceb53bce8f40e9
ferrufino <gustavo.fdlf@gmail.com>
2016-12-04 15:25:30 -0600

    Cleans files

--- Angular/t247-app/src/app/app.routing.ts
+++ Angular/t247-app/src/app/app.routing.ts
@@ -2,26 +2,23 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent }      from './principal-components/login-component/login.component';
-import {HomeComponent} from "./principal-components/home-component/home.component";
 import {AppComponent} from "./app.component";
 import {ProfileComponent} from "./general-components/profile/profile.component";
-import {TopicsEditComponent} from "./admin-components/topics-edit/topics-edit.component";
-import {CoursesEditComponent} from "./admin-components/courses-edit/courses-edit.component";
 import {GroupComponent} from "./professor-components/group/group.component";
 import { GroupResolve }   from "./services/group-resolve.service";
-import {UserEditComponent} from "./admin-components/users-edit/users-edit.component";
 import {SubmitProblem} from "./general-components/submit-problem/submit-problem.component";
-import {FirstLoginComponent} from "./student-components/first-login.component";
 import {ProblemDetailsComponent} from "./general-components/problem-details/problem-details.component";
 import {CreateProblem} from "./general-components/create-problem/create-problem.component";
 
-import {AdminHomeComponent} from "./admin-components2/admin-home/admin-home.component";
+
+import {AdminHomeComponent} from "./admin-components/admin-home/admin-home.component";
 import {AdminGuard} from "./services/admin.guard";
 
-import {ProfessorHomeComponent} from "./professor-components-2/professor-home/professor-home.component";
+//import {ProfessorHomeComponent} from "./professor-components-2/professor-home/professor-home.component";
+import {ProfessorHomeComponent} from "./professor-components/professor-home/professor-home.component";
 import {ProfessorGuard} from "./services/professor.guard";
 
-import {StudentHomeComponent} from "./student-components-2/student-home/student-home.component";
+import {StudentHomeComponent} from "./student-components/student-home/student-home.component";
 import {StudentGuard} from "./services/student.guard";
 
 import {LoginGuard} from "./services/login.guard";
