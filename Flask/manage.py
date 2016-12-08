6c5cabefece4328b7b57750b88e637507a16052f
Server Admin <ejzardainc@gmail.com>
2016-12-08 15:59:28 -0600

    Adds create admin task

--- Flask/manage.py
+++ Flask/manage.py
@@ -1,6 +1,7 @@
 import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
+from create_admin import CreateAdmin
 from app import app, db
 import config
 
@@ -11,7 +12,7 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 
 manager.add_command('db', MigrateCommand)
-
+manager.add_command('create_admin', CreateAdmin)
 
 if __name__ == '__main__':
     manager.run()
